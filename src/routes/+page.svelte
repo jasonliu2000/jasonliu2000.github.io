632e7fea5e75acae642291565e2d40390d7153c9
Jason Liu <jasonliu00@gmail.com>
2024-11-16 22:27:28 -0500

    Open links in new tab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 <p>Software engineer based in Massachusetts.</p>
 <p>Graduated from Boston College in 2022 with a degree in Mathematics and Economics.</p>
 
-<a href="https://www.linkedin.com/in/jasonliu-bc2022/">LinkedIn</a> | 
-<a href="https://github.com/jasonliu2000">Github</a> |
+<a target="_blank" href="https://www.linkedin.com/in/jasonliu-bc2022/">LinkedIn</a> | 
+<a target="_blank" href="https://github.com/jasonliu2000">Github</a> |
 <a href="mailto:jasonliu00@gmail.com"> Email me</a>
 
 <style>
