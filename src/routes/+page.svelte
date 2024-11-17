a7c8e4c64d9f4a55ff8e2715e7d8d2189f73a9cf
Jason Liu <jasonliu00@gmail.com>
2024-11-16 23:16:20 -0500

    Add icons, customize font and placement

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,41 @@
-<h1>jason liu</h1>
+<script>
+  import IconLinkedIn from '~icons/cib/linkedin';
+  import IconGithub from '~icons/cib/github';
+  import IconEmail from '~icons/carbon/email';
+</script>
 
-<p>Software engineer based in Massachusetts.</p>
-<p>Graduated from Boston College in 2022 with a degree in Mathematics and Economics.</p>
+<main>
+  <h1>jason liu</h1>
 
-<a target="_blank" href="https://www.linkedin.com/in/jasonliu-bc2022/">LinkedIn</a> | 
-<a target="_blank" href="https://github.com/jasonliu2000">Github</a> |
-<a href="mailto:jasonliu00@gmail.com"> Email me</a>
+  <p>Software engineer based in Massachusetts.</p>
+  <p>Graduated from Boston College in 2022 with a degree in Mathematics and Economics.</p>
+  <br>
+  <a target="_blank" href="https://www.linkedin.com/in/jasonliu-bc2022/"><IconLinkedIn /></a> 
+  <a target="_blank" href="https://github.com/jasonliu2000"><IconGithub /></a>
+  <a href="mailto:jasonliu00@gmail.com"><IconEmail /></a>
+</main>
 
 <style>
   h1 {
     color: darkblue;
+    font-size: 100px;
+  }
+
+  p {
+    font-size: 25px;
+  }
+
+  a {
+    color: gray;
+    font-size: 25px;
+    margin-right: 15px;
+  }
+  a:hover {
+    color: black;
+  }
+
+  main {
+    margin: auto;
+    width: 60%;
   }
 </style>
