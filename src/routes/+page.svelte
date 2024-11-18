a2805056b14a5c57044345025601030496e2000f
Jason Liu <jasonliu00@gmail.com>
2024-11-18 11:03:04 -0500

    Revert dummy commit change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 <main>
   <h1>jason liu</h1>
 
-  <p>Software engineer based on Mars.</p>
+  <p>Software engineer based in Massachusetts.</p>
   <p>Graduated from Boston College in 2022 with a degree in Mathematics and Economics.</p>
   <br>
   <a target="_blank" href="https://www.linkedin.com/in/jasonliu-bc2022/"><IconLinkedIn /></a> 
